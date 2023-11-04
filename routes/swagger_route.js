94e72022ab99606c6c4d98b69310d80c2d22095c
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2023-11-04 15:39:54 -0400

    modified

--- routes/swagger_route.js
+++ routes/swagger_route.js
@@ -3,6 +3,6 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../docs/swagger.json');
 
 router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+router.get('/', swaggerUi.setup(swaggerDocument,false, {}, null, null, null));
 
 module.exports = router;
