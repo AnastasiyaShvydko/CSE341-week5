2c3e5320fe1ebf708fafbdd420b20de079cc7967
AnastasiyaShvydko <84057111+AnastasiyaShvydko@users.noreply.github.com>
2023-11-04 16:09:30 -0400

    changed host

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const route = require ('express').Router();
 
 route.get('/google', passport.authenticate('google', { scope: ['profile'] }));
 route.get('/google/callback', passport.authenticate('google'),(req,res) => {
-    res.redirect('/planets/all');
+    res.redirect('/api-docs');
 });
 route.get('/logout',(req,res)=>{
     req.logout(function(err) {
